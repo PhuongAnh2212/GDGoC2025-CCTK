1c4ff9577ba48936fd5872f35d86c575d82747a6
leoleo <qp713223@gmail.com>
2025-04-10 16:24:33 +0700

    new change

--- ai-purpose-lab/src/components/main.jsx
+++ ai-purpose-lab/src/components/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< Updated upstream
 import './gochoctap.css'
 
+=======
+>>>>>>> Stashed changes
 import './Card1.jsx'
 import App from './gochoctap.jsx'
 
